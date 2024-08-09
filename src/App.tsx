149a8bba51e3a4917b1b76efcec49a3fe0ac1519
polozovaanastasia <polozova.anastasia@gmail.com>
2024-08-09 18:48:45 +0500

    test.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+
+
+    return (
+        <div className="App">
+            <h1>Weather App</h1>
+        </div>
+    );
+}
+
+export default App;
