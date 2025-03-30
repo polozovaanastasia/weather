7ca7e04f6d05cc29ff176e53479231de1d680c45
polozovaanastasia <polozova.anastasia@gmail.com>
2025-03-30 17:34:58 +0500

    add webpack plugins HtmlWebpackPlugin, ProgressPlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
     mode: "development",
     entry: path.resolve(__dirname, "src", "index.js"),
     output: {
-        filename: "[name].[contenthash].js", // todo: читаем про contenthash и кэширование.
+        filename: "[name].[contenthash].js", // contenthash, чтобы избежать проблем с кэшированием.
         path: path.resolve(__dirname, "build"),
+        clean: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html"),
+        }),
+        new webpack.ProgressPlugin(),
+    ],
 };
